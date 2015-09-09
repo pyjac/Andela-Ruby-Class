f4144c196cd630686298a87039f657f04e751997
pyjac <oyebanji05@gmail.com>
2015-09-09 16:52:28 +0000

    add

---
+++ binary_search.rb
@@ -0,0 +1,21 @@
+
+class Array
+
+    def binary_search?(target)
+       lo = 0; hi = self.length
+       while lo < hi
+          mid = lo + (hi-lo)/2
+          if self[mid] == target
+             return true            
+          elsif (self[mid] < target)
+             lo = mid + 1
+          else
+             hi = mid - 1
+         end
+       
+        end
+        false
+    end
+end
+
+p [1,2,3].binary_search?(4)
